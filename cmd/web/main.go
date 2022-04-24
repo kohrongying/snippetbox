cedbb84de33e5a37af72ce063eb3037729363baf
rongying <kohrongying@gmail.com>
2022-04-24 23:05:46 +0800

    Serve static files.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,9 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	// slash is a catch all. eg. /foo, /bash --> home
 	mux.HandleFunc("/", home) 
+	
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
