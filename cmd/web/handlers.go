bed97b0d22f2ceb77e82a7f06bb94b1a32070df2
rongying <kohrongying@gmail.com>
2022-05-06 00:05:48 +0800

    Parse form in create post handler.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -62,17 +62,27 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request)  {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// EDIT: done by httprouter
-	// check request method
+	// EDIT: done by httprouter 
 	// if r.Method != http.MethodPost {
 	// 	w.Header().Set("Allow", "POST")
 	// 	app.clientError(w, http.StatusMethodNotAllowed)
 	// 	return
 	// }
 
-	title := "0 turt"
-	content := "Turt is climbing mount fuji.\nSlowly and slowly!"
-	expires := 2
+	// Parse form
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content") //returns string
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
 	
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
@@ -80,5 +90,5 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 	app.infoLog.Printf("id is %d", id)
-	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
