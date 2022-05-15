b64b90c05d394b7d2a23f51fee8a870ea46638c6
rongying <kohrongying@gmail.com>
2022-05-15 11:02:00 +0800

    Add protected routes to middleware.

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -45,8 +45,21 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 			}
 		}()
 
+		next.ServeHTTP(w, r)	
+	})	
+}
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		// If authenticated, set cache control to no store so pages requiring auth
+		// are not stored in browser cache or intermediary cache
+		w.Header().Add("Cache-Control", "no-store")
+
 		next.ServeHTTP(w, r)
-		
 	})
-	
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,17 +20,13 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/")) // static file server
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	// middleware chain for dynamic routes
+	// middleware chain for dynamic routes with session manager
 	dynamic := alice.New(app.sessionManager.LoadAndSave)
 
 	// same as router.Handle("/", &home{}) or
 	// same as router.Handle("/", http.HandlerFunc(home))
 	// home -> func (h *home) ServeHTTP(w, r)
 	// servemux doesnt support clean url (view/:id), using httprouter
-	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView)) 
-	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
@@ -40,7 +36,12 @@ func (app *application) routes() http.Handler {
 	// slash is a catch all. eg. /foo, /bash --> home
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 
-	
+	// protected (authenticated-only) application routes using new requireAuthentication middleware
+	protected := dynamic.Append(app.requireAuthentication)
+	router.Handler(http.MethodGet, "/snippet/view/:id", protected.ThenFunc(app.snippetView)) 
+	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+
 	// create middleware chain
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
